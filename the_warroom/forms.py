b342f85a0e8d8ab0d469d74796e3f52b4fb3ed8f
Tim <timbob1907@me.com>
2025-01-13 10:59:07 -0800

    trying other options

--- the_warroom/forms.py
+++ the_warroom/forms.py
@@ -386,23 +386,23 @@ class TurnScoreCreateForm(forms.ModelForm):
         fields = ['id', 'turn_number', 'faction_points', 'crafting_points', 'battle_points', 'other_points', 'dominance']
     faction_points = forms.IntegerField(
         required=False, 
-        widget=forms.NumberInput(attrs={'type': 'text', 'inputmode': 'numeric', 'step': 1}),
+        widget=forms.NumberInput(attrs={'type': 'tel', 'inputmode': 'tel', 'step': 1}),
         initial=0
         )
     crafting_points = forms.IntegerField(
         required=False, 
-        widget=forms.NumberInput(attrs={'type': 'text', 'inputmode': 'numeric', 'step': 1}),
+        widget=forms.NumberInput(attrs={'type': 'number', 'inputmode': 'text', 'step': 1}),
         initial=0
         )
     battle_points = forms.IntegerField(
         required=False, 
-        widget=forms.NumberInput(attrs={'type': 'text', 'inputmode': 'numeric', 'step': 1}),
+        widget=forms.NumberInput(attrs={'type': 'number', 'inputmode': 'decimal', 'step': 1}),
         initial=0
         )
     other_points = forms.IntegerField(
         required=False, 
         widget=forms.NumberInput(attrs={
-            'type': 'text',
+            'type': 'number',
             'inputmode': 'numeric',
             'step': 1
         }),
